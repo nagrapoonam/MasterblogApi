5f86e4cf2bb223fb590cefa76530f70e98a5ea22
nagrapoonam <nagra.poonam1@gmail.com>
2023-07-01 21:43:10 -0400

    new project

---
+++ backend/backend_app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # This will enable CORS for all routes
+
+POSTS = [
+    {"id": 1, "title": "First post", "content": "This is the first post."},
+    {"id": 2, "title": "Second post", "content": "This is the second post."},
+]
+
+
+@app.route('/api/posts', methods=['GET'])
+def get_posts():
+    return jsonify(POSTS)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5002, debug=True)
---
+++ frontend/static/main.js
@@ -0,0 +1,72 @@
+// Function that runs once the window is fully loaded
+window.onload = function() {
+    // Attempt to retrieve the API base URL from the local storage
+    var savedBaseUrl = localStorage.getItem('apiBaseUrl');
+    // If a base URL is found in local storage, load the posts
+    if (savedBaseUrl) {
+        document.getElementById('api-base-url').value = savedBaseUrl;
+        loadPosts();
+    }
+}
+
+// Function to fetch all the posts from the API and display them on the page
+function loadPosts() {
+    // Retrieve the base URL from the input field and save it to local storage
+    var baseUrl = document.getElementById('api-base-url').value;
+    localStorage.setItem('apiBaseUrl', baseUrl);
+
+    // Use the Fetch API to send a GET request to the /posts endpoint
+    fetch(baseUrl + '/posts')
+        .then(response => response.json())  // Parse the JSON data from the response
+        .then(data => {  // Once the data is ready, we can use it
+            // Clear out the post container first
+            const postContainer = document.getElementById('post-container');
+            postContainer.innerHTML = '';
+
+            // For each post in the response, create a new post element and add it to the page
+            data.forEach(post => {
+                const postDiv = document.createElement('div');
+                postDiv.className = 'post';
+                postDiv.innerHTML = `<h2>${post.title}</h2><p>${post.content}</p>
+                <button onclick="deletePost(${post.id})">Delete</button>`;
+                postContainer.appendChild(postDiv);
+            });
+        })
+        .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+}
+
+// Function to send a POST request to the API to add a new post
+function addPost() {
+    // Retrieve the values from the input fields
+    var baseUrl = document.getElementById('api-base-url').value;
+    var postTitle = document.getElementById('post-title').value;
+    var postContent = document.getElementById('post-content').value;
+
+    // Use the Fetch API to send a POST request to the /posts endpoint
+    fetch(baseUrl + '/posts', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title: postTitle, content: postContent })
+    })
+    .then(response => response.json())  // Parse the JSON data from the response
+    .then(post => {
+        console.log('Post added:', post);
+        loadPosts(); // Reload the posts after adding a new one
+    })
+    .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+}
+
+// Function to send a DELETE request to the API to delete a post
+function deletePost(postId) {
+    var baseUrl = document.getElementById('api-base-url').value;
+
+    // Use the Fetch API to send a DELETE request to the specific post's endpoint
+    fetch(baseUrl + '/posts/' + postId, {
+        method: 'DELETE'
+    })
+    .then(response => {
+        console.log('Post deleted:', postId);
+        loadPosts(); // Reload the posts after deleting one
+    })
+    .catch(error => console.error('Error:', error));  // If an error occurs, log it to the console
+}
